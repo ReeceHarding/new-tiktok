6043d478bfc06acf41eea181edeb827eb9414e13
Reece Harding <reeceharding@gmail.com>
2025-02-04 23:05:58 -0600

    Initial Commit

---
+++ New-AI/New_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_AIApp.swift
+//  New-AI
+//
+//  Created by Reece Harding on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct New_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New-AITests/New_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  New_AITests.swift
+//  New-AITests
+//
+//  Created by Reece Harding on 2/4/25.
+//
+
+import Testing
+@testable import New_AI
+
+struct New_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
